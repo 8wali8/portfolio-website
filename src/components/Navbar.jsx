bd7947ae099c2cbe89362e2ec84abd9e2eca609d
8wali8 <ujjawalprasad111@gmail.com>
2025-10-05 22:35:06 -0400

    added analytics'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> Ujjawal's </span>{" "}
+            <span className="text-glow text-foreground"> Ujjawals </span>{" "}
             Portfolio
           </span>
         </a>
